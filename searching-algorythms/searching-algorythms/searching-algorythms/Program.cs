ecf705c3d9a34a75c6b141a759d1d7038789d586
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2020-05-25 23:35:30 +0200

    finish task 1 and task 2

--- searching-algorythms/searching-algorythms/searching-algorythms/Program.cs
+++ searching-algorythms/searching-algorythms/searching-algorythms/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Diagnostics;
 
 namespace searching_algorythms
 {
     class Program
     {
-        static int[] startArray = new int[50000];
-        static int[] arrayAsc = GenerateAscendingArray(startArray);
-        static int[] arrayDesc = GenerateDescendingArray(startArray);
-        //static int[] arrayRnd = GenerateRandomArray(startArray);
-        static int[] arrayV = GenerateVArray(startArray);
+        static Random rnd = new Random(Guid.NewGuid().GetHashCode());
 
         static void Main(string[] args)
         {
+            SortingPart1();
+
             Thread TesterThread = new Thread(Program.Tester, 8 * 1024 * 1024);
             TesterThread.Start();
             TesterThread.Join();
@@ -95,6 +94,7 @@ namespace searching_algorythms
         }
         static void HeapSort(int[] t)
         {
+            //time complexity O(n log n) - average case
             for (int i = 1; i < t.Length; i++)
             {
                 int Buf = t[i];
@@ -112,6 +112,7 @@ namespace searching_algorythms
         }
         static void CoctailSort(int[] t)
         {
+            //time complexity O(n^2) - average case
             int Left = 1, Right = t.Length - 1, k = t.Length - 1;
             do
             {
@@ -131,12 +132,77 @@ namespace searching_algorythms
                 Right = k - 1;
             } while (Left <= Right);
         }
+        static void QuicksortRecursive(int[] t, int l, int p)
+        {
+            int i, j, x;
+            i = l;
+            j = p;
+            x = t[(l + p) / 2];
+            do
+            {
+                while (t[i] < x) i++;
+                while (x < t[j]) j--;
+                if (i <= j)
+                {
+                    int buf = t[i]; t[i] = t[j]; t[j] = buf;
+                    i++; j--;
+                }
+            } while (i <= j);
+            if (l < j) QuicksortRecursive(t, l, j);
+            if (i < p) QuicksortRecursive(t, i, p);
+        }
+        static void Sorting(int[] t)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            InsertionSort(t);
+            stopwatch.Stop();
+            Console.Write("{0};", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            SelectionSort(t);
+            stopwatch.Stop();
+            Console.Write("{0};", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            HeapSort(t);
+            stopwatch.Stop();
+            Console.Write("{0};", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            CoctailSort(t);
+            stopwatch.Stop();
+            Console.Write("{0}\n", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+        }
+
+        static void SortingPart1()
+        {
+            for (int arraySize = 50000; arraySize <= 200000; arraySize += 50000)
+            {
+
+                Console.WriteLine("{0}\narray_type;insertion;selection;heap;coctail", arraySize);
+                int[] startArray = new int[arraySize];
+                Console.Write("ascending_array;");
+                Sorting(GenerateAscendingArray(startArray));
+                Console.Write("descending_array;");
+                Sorting(GenerateDescendingArray(startArray));
+                Console.Write("random_array;");
+                Sorting(GenerateRandomArray(startArray, rnd));
+                Console.Write("v_array;");
+                Sorting(GenerateVArray(startArray));
+            }
+
+        }
+
 
         static void Tester()
         {
-            InsertionSort(arrayAsc);
-        }
-        
 
+        }
     }
 }
